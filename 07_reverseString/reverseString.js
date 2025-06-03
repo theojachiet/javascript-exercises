c9e8b351001b1bccdeff84bb06cb541d41c0fbb0
Teoslip <tjachiet@gmail.com>
2025-06-03 11:17:40 +0200

    Complete exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let tempArr = [];
+    let arr = string.split('');
+    for (let i = arr.length; i >= 0; i--) {
+        tempArr.push(arr[i]);
+    }
+    let result = tempArr.join('');
+    return result;
 };
+
+console.log(reverseString('hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
