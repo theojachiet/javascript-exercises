1c0bad8714ab5f58727fd43ad1af6bae02dcbf97
Teoslip <tjachiet@gmail.com>
2025-06-03 11:39:02 +0200

    Complete exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+
+    for (const arg of theArgs) {
+        array = array.filter(elem => elem !== arg)
+    }
+    return array;
 };
 
 // Do not edit below this line
