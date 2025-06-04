8176acadaedb2913f7040b3bacb2e3efcd6907f6
Teoslip <tjachiet@gmail.com>
2025-06-04 19:50:34 +0200

    Complete exercise 16!

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
+const date = new Date();
 
+const findTheOldest = function(people) {
+    let result = people.sort((a, b) => ((b.yearOfDeath || date.getFullYear()) - b.yearOfBirth) - ((a.yearOfDeath || date.getFullYear()) - a.yearOfBirth));
+    return result[0];
 };
 
 // Do not edit below this line
