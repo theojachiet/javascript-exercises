eecc80f5944ec06e04f6b793aeb9dc5e1d8a38bf
Teoslip <tjachiet@gmail.com>
2025-06-04 19:16:26 +0200

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz0123456789';
 
+const palindromes = function (str) {
+    
+    str = transformLetters(str);
+    let reverse = str.split('').reverse().join('');
+    if (str === reverse) return true;
+    else return false;
 };
+
+function transformLetters(str) {
+    let parts = str.split('');
+    let alphaParts = [];
+    for (char of parts) {
+        if (alphanumeric.includes(char)) {
+            alphaParts.push(char);
+        }
+    }
+    str = alphaParts.join('');
+    return str = str.toLowerCase();
+}
+
+console.log(palindromes('sdfdsfsdffggfd'));
 
 // Do not edit below this line
 module.exports = palindromes;
