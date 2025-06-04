3095a480836dfae8a8b3fcd56bb7d3603d59d6bd
Teoslip <tjachiet@gmail.com>
2025-06-04 19:29:25 +0200

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const goldenRatio =  1.618034; 
+    num = Number(num);
+    if (num < 0) return 'OOPS';
+    return Math.floor(((goldenRatio ** num) - (1 - goldenRatio) ** num) / Math.sqrt(5));
 };
 
 // Do not edit below this line
