87f64109791c38c92f1bfa3ad093ff87b4cb7a0e
Teoslip <tjachiet@gmail.com>
2025-06-04 19:33:29 +0200

    Complete exercise 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let result = [];
+    result = arr.map(({title, author}) => title);
+    return result;
 };
 
 // Do not edit below this line
